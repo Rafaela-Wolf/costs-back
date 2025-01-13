3660773e1231a267b57c36467eecdf84eb8a332a
Rafaela Wolf <rafaelawolf52@gmail.com>
2025-01-13 16:59:32 -0300

    configuração para rodar no vercell

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ server.use(middlewares);
 server.use(router);
 
 const port = process.env.PORT || 3000;
+
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
