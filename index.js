06fc5e44cdb068a93a6a208c7917d50f643c13aa
Rafaela Wolf <rafaelawolf52@gmail.com>
2025-01-14 16:52:54 -0300

    adicionando os demais endpoints

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ app.get('/api/projects', (req, res) => {
   res.json(db.projects || []);
 });
 
+app.post('/api/projects', (req, res) => {
+  res.json(db.projects || []);
+});
+
+app.patch('/api/projects/:project.id', (req, res) => {
+  res.json(db.projects || []);
+});
+
+app.delete('/api/projects/:id', (req, res) => {
+  res.json(db.projects || []);
+});
+
+// app.post('/api/projects', (req, res) => {
+//   const newProject = req.body;
+
+//   if (!newProject || !newProject.name || !newProject.budget) {
+//       return res.status(400).json({ message: "Invalid project data" });
+//   }
+
+//   db.projects.push(newProject);
+
+//   res.status(201).json(newProject);
+// });
+
 // Endpoint para obter os dados das categorias 
 app.get('/api/categories', (req, res) => {
   res.json(db.categories || []);
